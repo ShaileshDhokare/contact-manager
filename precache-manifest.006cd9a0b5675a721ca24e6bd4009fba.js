77414e65326d4367d16c61da2f3382db32b1314c
ShaileshDhokare <shailesh19sd@gmail.com>
2019-07-21 14:48:02 +0530

    Updates

---
+++ precache-manifest.006cd9a0b5675a721ca24e6bd4009fba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef57ce80bf45d915e943e333996fb0ad",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "3f68151bbd147b89299b",
+    "url": "/contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "79d13cacffb404de6529",
+    "url": "/contact-manager/static/css/main.642e4981.chunk.css"
+  },
+  {
+    "revision": "3f68151bbd147b89299b",
+    "url": "/contact-manager/static/js/2.b3abeda2.chunk.js"
+  },
+  {
+    "revision": "79d13cacffb404de6529",
+    "url": "/contact-manager/static/js/main.1f9af6f1.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
